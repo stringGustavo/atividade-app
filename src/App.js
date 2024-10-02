2eda46274d27d2167c731ff5ebdbba389c662722
Gustavo Santos <gustavo.st971@gmail.com>
2024-10-02 05:46:51 -0300

    adicionando useState e Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 
+let initialState = [
+  {
+    "id": 1,
+    "description": "Primeira Atividade"
+  },
+  {
+    "id": 2,
+    "description": "Segunda Atividade"
+  },
+]
+
 function App() {
-  const atividade = [
-    {
-      "id": 1,
-      "descricao": "Primeira Atividade"
+  const [tasks, setTasks] = useState(initialState);
+
+  function addTask(event) {
+    event.preventDefault();
+
+    const task = {
+      "id": document.getElementById('id').value,
+      "description": document.getElementById('description').value
     }
-  ]
+
+    setTasks([...tasks, { ...task }]);
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          <li>{atividade[0].id} {atividade[0].descricao}</li>
-          <li onClick={ () => alert('Teste')}>Segunda Atividade</li>
-          <li>Terceira Atividade</li>
-          <li>Quarta Atividade</li>
-        </ul>
-      </header>
-    </div>
+    <>
+      <form className='row g-3'>
+        <div className="col-md-6">
+          <label placeholder='id' className="form-label">ID</label>
+          <input id='id' type="text" className="form-control" />
+        </div>
+        <div className="col-md-6">
+          <label placeholder='id' className="form-label">Descrição</label>
+          <input id='description' type="text" className="form-control" />
+        </div>
+        <div className="col-12">
+          <button className="btn btn-outline-success" onClick={addTask}>
+            + Task
+          </button>
+        </div>
+      </form>
+      <div className="mt-3">
+        {tasks.map(atv => (
+        <div key={atv.id} className="card mb-2 shadow-sm">
+            <div className="card-body">
+              <p className="card-text">{atv.id} - {atv.description}</p>
+            </div>
+        </div>
+        ))}
+      </div>
+    </>
   );
 }
 
