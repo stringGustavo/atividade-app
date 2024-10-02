c0aa991704dc5b13155ff0710c95107447bf3b76
Gustavo Santos <gustavo.st971@gmail.com>
2024-10-02 03:44:31 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const atividade = [
+    {
+      "id": 1,
+      "descricao": "Primeira Atividade"
+    }
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          <li>{atividade[0].id} {atividade[0].descricao}</li>
+          <li onClick={ () => alert('Teste')}>Segunda Atividade</li>
+          <li>Terceira Atividade</li>
+          <li>Quarta Atividade</li>
+        </ul>
       </header>
     </div>
   );
